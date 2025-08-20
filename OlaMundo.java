3302d8b385cc63c82b71408991d47d40456aa9c5
HSA995 <henriquesouto995@gmail.com>
2025-08-20 12:22:48 -0300

    20/08 java

---
+++ OlaMundo.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+class OlaMundo {
+
+   public static void main(String[] args){
+	   System.out.println("Olá Mundo");
+      JOptionPane.showMessageDialog(null, "Ola Mundo");
+      System.exit(0);
+
+   }
+
+}
